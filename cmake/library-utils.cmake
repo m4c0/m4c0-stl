17976672cff78b09c9ab5f8560f1e5f6beb0a2b6
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-05 09:09:55 +0000

    Refactoring m4c0_add_library

--- cmake/library-utils.cmake
+++ cmake/library-utils.cmake
@@ -1,16 +1,28 @@
+function(_m4c0_add_library)
+  set(one_value_args ALIAS SCOPE TARGET TYPE)
+  set(multi_value_args LIBRARIES)
+  cmake_parse_arguments(MAL "" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+  add_library(${MAL_TARGET} ${MAL_TYPE})
+  target_compile_features(${MAL_TARGET} ${MAL_SCOPE} cxx_std_20)
+  target_include_directories(${MAL_TARGET} ${MAL_SCOPE} inc)
+  target_link_libraries(${MAL_TARGET} ${MAL_SCOPE} ${MAL_LIBRARIES})
+
+  add_library(${MAL_ALIAS} ALIAS ${MAL_TARGET})
+endfunction()
+
 function(m4c0_add_interface)
   set(one_value_args TARGET ALIAS)
   set(multi_value_args LIBRARIES)
   cmake_parse_arguments(MAL "" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-  add_library(${MAL_TARGET} INTERFACE)
-  target_compile_features(${MAL_TARGET} INTERFACE cxx_std_20)
-  target_include_directories(${MAL_TARGET} INTERFACE inc)
-  target_link_libraries(${MAL_TARGET} INTERFACE ${MAL_LIBRARIES})
-
-  add_library(${MAL_ALIAS} ALIAS ${MAL_TARGET})
+  _m4c0_add_library(
+    TARGET ${MAL_TARGET}
+    ALIAS ${MAL_ALIAS}
+    LIBRARIES ${MAL_LIBRARIES}
+    TYPE INTERFACE
+    SCOPE INTERFACE)
 endfunction()
-
 
 function(m4c0_add_library)
   set(options OBJCXX)
@@ -18,10 +30,13 @@ function(m4c0_add_library)
   set(multi_value_args SOURCES LIBRARIES)
   cmake_parse_arguments(MAL "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-  add_library(${MAL_TARGET} STATIC ${MAL_SOURCES})
-  target_compile_features(${MAL_TARGET} PUBLIC cxx_std_20)
-  target_include_directories(${MAL_TARGET} PUBLIC inc)
-  target_link_libraries(${MAL_TARGET} PUBLIC ${MAL_LIBRARIES})
+  _m4c0_add_library(
+    TARGET ${MAL_TARGET}
+    ALIAS ${MAL_ALIAS}
+    LIBRARIES ${MAL_LIBRARIES}
+    TYPE STATIC
+    SCOPE PUBLIC)
+  target_sources(${MAL_TARGET} PRIVATE ${MAL_SOURCES})
 
   if(MAL_OBJCXX)
     target_compile_options(${MAL_TARGET} PRIVATE -fmodules -fobjc-arc)
@@ -31,7 +46,5 @@ function(m4c0_add_library)
       OBJCXX_EXTENSIONS FALSE
       )
   endif()
-
-  add_library(${MAL_ALIAS} ALIAS ${MAL_TARGET})
 endfunction()
 
