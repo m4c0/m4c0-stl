c1fabdaa98d658d5aa1cf8b469d37e3876dd4062
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-11 08:25:44 +0000

    [maze] moving code around

--- maze/tst/view_grid.cpp
+++ maze/tst/view_grid.cpp
@@ -2,10 +2,6 @@
 #include "m4c0/test.hpp"
 
 using namespace m4c0::maze::model;
-static constexpr const auto grid_width = 4;
-static constexpr const auto grid_height = 3;
-static constexpr const auto array_width = grid_width * 2 - 1;
-static constexpr const auto array_size = array_width * (grid_height * 2 - 1);
 
 template<unsigned GridW, unsigned GridH>
 class grid {
@@ -13,9 +9,14 @@ class grid {
   static constexpr const auto grid_height = GridH;
   static constexpr const auto room_count = grid_width * grid_height;
 
-  graph<room<int, 4>, room_count> m_graph {};
+  using room_t = room<int, 4>;
+
+  graph<room_t, room_count> m_graph {};
 
 public:
+  static constexpr const auto array_width = grid_width * 2 - 1;
+  static constexpr const auto array_size = array_width * (grid_height * 2 - 1);
+
   constexpr grid() {
     for (int y = 0, i = 0; y < grid_height; ++y) {
       for (int x = 0; x < grid_width; ++x, ++i) {
@@ -30,18 +31,22 @@ public:
 
   template<typename Fn>
   void visit_grid(Fn fn) const {
-    m_graph.visit_rooms([&](const auto * r) {
+    m_graph.visit_rooms([&](const room_t * r) {
       fn(r->data());
     });
-    m_graph.visit_walls([&](const auto * ra, const auto * rb) {
+    m_graph.visit_walls([&](const room_t * ra, const room_t * rb) {
       fn((ra->data() + rb->data()) / 2);
     });
   }
 };
 
+static constexpr const auto grid_width = 4;
+static constexpr const auto grid_height = 3;
+using grid_t = grid<grid_width, grid_height>;
+
 go_bandit([] { // NOLINT
-  describe("maze::view::grid", [] {
-    it("works", [] {
+  describe("maze::model::grid", [] {
+    it("can be visited by index", [] {
       // Expected:
       // 0*1*2 3
       //   *   *
@@ -50,8 +55,7 @@ go_bandit([] { // NOLINT
       // 8*9*A B
       enum { R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, RA, RB };
 
-      grid<grid_width, grid_height> model {};
-
+      grid_t model {};
       model.double_link(R0, R1);
       model.double_link(R1, R2);
       model.double_link(R1, R5);
@@ -64,9 +68,9 @@ go_bandit([] { // NOLINT
       model.double_link(R7, R3);
       model.double_link(R7, RB);
 
-      std::array<char, array_size + 1> result {};
+      std::array<char, grid_t::array_size + 1> result {};
       std::fill(std::begin(result), std::end(result), ' ');
-      result[array_size] = 0;
+      result[grid_t::array_size] = 0;
 
       model.visit_grid([&](unsigned index) {
         result.at(index) = '*';
