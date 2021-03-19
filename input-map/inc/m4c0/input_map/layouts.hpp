47f058acdb25ee63fb2a08f01eb3848b6cb459cd
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-19 09:24:48 +0000

    [input_map] introducing names to buttons

--- input-map/inc/m4c0/input_map/layouts.hpp
+++ input-map/inc/m4c0/input_map/layouts.hpp
@@ -2,34 +2,75 @@
 
 #include "m4c0/function.hpp"
 
+namespace m4c0::input_map {
+  enum class axis {
+    LPAD_V,
+    LPAD_H,
+
+    RPAD_V,
+    RPAD_H,
+  };
+  enum class buttons {
+    LPAD,
+    RPAD,
+
+    DPAD_LEFT,
+    DPAD_UP,
+    DPAD_RIGHT,
+    DPAD_DOWN,
+
+    X,
+    Y,
+    A,
+    B
+  };
+}
 namespace m4c0::input_map::layout {
   namespace details {
-    using axis_callback = function<void(float)>;
-    using button_callback = function<void(bool)>;
+    template<typename ValueT, auto Name>
+    class control : public function<void(ValueT)> {
+      using parent_t = function<void(ValueT)>;
 
+    public:
+      using parent_t::parent_t;
+
+      [[nodiscard]] constexpr auto name() const noexcept {
+        return Name;
+      }
+    };
+
+    template<auto Name>
+    using axis_callback = control<float, Name>;
+    template<auto Name>
+    using button_callback = control<bool, Name>;
+
+    template<auto H, auto V>
     struct pad {
-      axis_callback horizontal;
-      axis_callback vertical;
+      axis_callback<H> horizontal;
+      axis_callback<V> vertical;
     };
 
-    struct pad_with_button : public pad {
-      button_callback center;
+    template<auto H, auto V, auto B>
+    struct pad_with_button : public pad<H, V> {
+      button_callback<B> center;
     };
 
+    template<auto L, auto T, auto R, auto B>
     struct quad_button {
-      button_callback left;
-      button_callback top;
-      button_callback right;
-      button_callback bottom;
+      button_callback<L> left;
+      button_callback<T> top;
+      button_callback<R> right;
+      button_callback<B> bottom;
     };
   }
 
-  struct base_controller {
-    details::pad_with_button lpad;
-    details::quad_button dpad;
+  class device {};
 
-    details::pad_with_button rpad;
-    details::quad_button buttons;
+  struct xbox : public device {
+    details::pad_with_button<axis::LPAD_H, axis::LPAD_V, buttons::LPAD> lpad;
+    details::quad_button<buttons::DPAD_LEFT, buttons::DPAD_UP, buttons::DPAD_RIGHT, buttons::DPAD_DOWN> dpad;
+
+    details::pad_with_button<axis::RPAD_H, axis::RPAD_V, buttons::RPAD> rpad;
+    details::quad_button<buttons::X, buttons::Y, buttons::B, buttons::A> buttons;
   };
-  struct xbox : public base_controller {};
 }
--- input-map/tst/dummy.cpp
+++ input-map/tst/dummy.cpp
@@ -1,7 +1,12 @@
 #include "m4c0/input_map/layouts.hpp"
 
+template<class Key>
+void show_on_screen(Key name) {
+}
+
 int main() {
   m4c0::input_map::layout::xbox x {};
   x.buttons.top = [](bool /*down*/) {
   };
+  show_on_screen(x.buttons.top.name());
 }
