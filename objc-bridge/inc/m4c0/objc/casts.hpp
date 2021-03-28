558b8f478af86f226243bfa1772aa6c3fc315cf8
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-28 19:29:31 +0100

    [objc-bridge] msg_send args

--- objc-bridge/inc/m4c0/objc/casts.hpp
+++ objc-bridge/inc/m4c0/objc/casts.hpp
@@ -2,22 +2,24 @@
 
 #include <objc/message.h>
 #include <objc/runtime.h>
+#include <utility>
 
 namespace m4c0::objc {
-  template<typename Ret>
-  static Ret objc_msg_send(void * obj, const char * sel_name) {
+  template<typename Ret, typename... Args>
+  static Ret objc_msg_send(void * obj, const char * sel_name, Args &&... args) {
     SEL sel = sel_getUid(sel_name);
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    return reinterpret_cast<Ret (*)(void *, SEL)>(objc_msgSend)(obj, sel);
+    return reinterpret_cast<Ret (*)(void *, SEL, Args...)>(objc_msgSend)(obj, sel, std::forward<Args>(args)...);
   }
-  template<typename Ret>
-  static Ret objc_msg_send_super(id self, SEL sel) {
+  template<typename Ret, typename... Args>
+  static Ret objc_msg_send_super(id self, SEL sel, Args &&... args) {
     objc_super s {};
     s.super_class = class_getSuperclass(object_getClass(self));
     s.receiver = self;
 
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    return reinterpret_cast<Ret (*)(objc_super *, void *, SEL)>(objc_msgSendSuper)(&s, self, sel);
+    return reinterpret_cast<Ret (*)(objc_super *, void *, SEL, Args...)>(
+        objc_msgSendSuper)(&s, self, sel, std::forward<Args>(args)...);
   }
 
   template<typename Ret, typename... Args>
