fbcec7bb13f8e7bf50f3c334e36c495fd4e4b34f
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-31 18:24:16 +0100

    [objc-bridge] perfect forwarding isn't a thing for msgSend

--- objc-bridge/inc/m4c0/objc/casts.hpp
+++ objc-bridge/inc/m4c0/objc/casts.hpp
@@ -15,37 +15,36 @@
 namespace m4c0::objc {
   namespace details {
     template<typename Ret, typename... Args>
-    static Ret call(void (*fn)(), void * obj, const char * sel_name, Args &&... args) {
+    static Ret call(void (*fn)(), void * obj, const char * sel_name, Args... args) {
       SEL sel = sel_getUid(sel_name);
       // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-      return reinterpret_cast<Ret (*)(void *, SEL, Args...)>(fn)(obj, sel, std::forward<Args>(args)...);
+      return reinterpret_cast<Ret (*)(void *, SEL, Args...)>(fn)(obj, sel, args...);
     }
   }
 
   template<typename Ret, typename... Args>
-  static auto objc_msg_send(void * obj, const char * sel_name, Args &&... args)
+  static auto objc_msg_send(void * obj, const char * sel_name, Args... args)
       -> std::enable_if_t<!std::is_floating_point_v<Ret> && !std::is_class_v<Ret>, Ret> {
-    return details::call<Ret>(&objc_msgSend, obj, sel_name, std::forward<Args>(args)...);
+    return details::call<Ret>(&objc_msgSend, obj, sel_name, args...);
   }
   template<typename Ret, typename... Args>
-  static auto objc_msg_send(void * obj, const char * sel_name, Args &&... args)
+  static auto objc_msg_send(void * obj, const char * sel_name, Args... args)
       -> std::enable_if_t<std::is_floating_point_v<Ret>, Ret> {
-    return details::call<Ret>(&objc_msgSend_fpret, obj, sel_name, std::forward<Args>(args)...);
+    return details::call<Ret>(&objc_msgSend_fpret, obj, sel_name, args...);
   }
   template<typename Ret, typename... Args>
-  static auto objc_msg_send(void * obj, const char * sel_name, Args &&... args)
+  static auto objc_msg_send(void * obj, const char * sel_name, Args... args)
       -> std::enable_if_t<std::is_class_v<Ret>, Ret> {
-    return details::call<Ret>(&objc_msgSend_stret, obj, sel_name, std::forward<Args>(args)...);
+    return details::call<Ret>(&objc_msgSend_stret, obj, sel_name, args...);
   }
   template<typename Ret, typename... Args>
-  static Ret objc_msg_send_super(id self, SEL sel, Args &&... args) {
+  static Ret objc_msg_send_super(id self, SEL sel, Args... args) {
     objc_super s {};
     s.super_class = class_getSuperclass(object_getClass(self));
     s.receiver = self;
 
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    return reinterpret_cast<Ret (*)(objc_super *, void *, SEL, Args...)>(
-        objc_msgSendSuper)(&s, self, sel, std::forward<Args>(args)...);
+    return reinterpret_cast<Ret (*)(objc_super *, void *, SEL, Args...)>(objc_msgSendSuper)(&s, self, sel, args...);
   }
 
   template<typename Ret, typename... Args>
