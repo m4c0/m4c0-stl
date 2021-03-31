a5c03be6bc890935f73a69334483191c28f3476f
Eduardo Costa <m4c0@users.noreply.github.com>
2021-03-31 17:54:04 +0100

    [ios-main] Metal protocol injection 🤘

--- ios-main/src/main.m
+++ ios-main/src/main.m
@@ -1,10 +1,13 @@
 #include "main.h"
 
+#import <MetalKit/MetalKit.h>
 #import <UIKit/UIKit.h>
 
 void objc_main(int argc, char ** argv, const char * delegate) {
   @autoreleasepool {
     Protocol * ui_application_delegate = @protocol(UIApplicationDelegate);
+    Protocol * mtk_view_delegate = @protocol(MTKViewDelegate);
+
     NSString * s = [NSString stringWithUTF8String:delegate];
     UIApplicationMain(argc, argv, nil, s);
   }
