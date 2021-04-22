515e8af2352680d703736c1cc0367480efe6c86c
Eduardo Costa <m4c0@users.noreply.github.com>
2021-04-22 18:53:16 +0100

    [vulkan] protecting cmd_buf_guard from misuse

--- vulkan-tools/inc/m4c0/vulkan/command_buffer_guard.hpp
+++ vulkan-tools/inc/m4c0/vulkan/command_buffer_guard.hpp
@@ -3,15 +3,23 @@
 #include "m4c0/vulkan/end_command_buffer.hpp"
 
 namespace m4c0::vulkan::tools {
-  struct command_buffer_guard : public cmd::base_command {
-    using cmd::base_command::base_command;
+  class command_buffer_guard {
+    VkCommandBuffer m_cmd_buf;
+
+  public:
+    explicit constexpr command_buffer_guard(VkCommandBuffer cb) : m_cmd_buf(cb) {
+    }
     ~command_buffer_guard() {
-      cmd::end_command_buffer(*this).now();
+      cmd::end_command_buffer(m_cmd_buf).now();
     }
 
     command_buffer_guard(command_buffer_guard &&) = delete;
     command_buffer_guard(const command_buffer_guard &) = delete;
     command_buffer_guard & operator=(command_buffer_guard &&) = delete;
     command_buffer_guard & operator=(const command_buffer_guard &) = delete;
+
+    [[nodiscard]] constexpr auto command_buffer() const noexcept {
+      return m_cmd_buf;
+    }
   };
 }
