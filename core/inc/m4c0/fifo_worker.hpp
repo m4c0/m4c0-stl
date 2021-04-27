62019040527dbbf3ca668a6a60f8862c7e912642
Eduardo Costa <m4c0@users.noreply.github.com>
2021-04-27 11:38:53 +0100

    [core] reset for fifo_worker

--- core/inc/m4c0/fifo_worker.hpp
+++ core/inc/m4c0/fifo_worker.hpp
@@ -10,13 +10,14 @@ namespace m4c0 {
   class fifo_worker {
     using action = function<void(Param...)>;
 
-    std::mutex m_mutex {};
+    std::mutex m_current_mutex {};
+    std::mutex m_flip_mutex {};
     std::vector<action> m_prev_q {};
     std::vector<action> m_cur_q {};
     std::vector<action> m_next_q {};
 
     void flip() {
-      std::lock_guard lock { m_mutex };
+      std::lock_guard lock { m_flip_mutex };
       m_prev_q = std::move(m_cur_q);
       m_cur_q = std::move(m_next_q);
       m_next_q.clear();
@@ -24,16 +25,24 @@ namespace m4c0 {
 
   public:
     void enqueue(action && a) {
-      std::lock_guard lock { m_mutex };
+      std::lock_guard lock { m_flip_mutex };
       m_next_q.push_back(std::forward<action>(a));
     }
     template<class... Args>
     void process(Args &&... p) {
       flip();
 
+      std::lock_guard lock { m_current_mutex };
       for (auto & q : m_cur_q) {
         q(std::forward<Args>(p)...);
       }
+    }
+    void reset() {
+      std::lock_guard lock1 { m_flip_mutex };
+      std::lock_guard lock2 { m_current_mutex };
+      m_prev_q.clear();
+      m_cur_q.clear();
+      m_next_q.clear();
     }
   };
 }
