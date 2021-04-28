5714906f06e896d58a311afe5505b712b582d0b5
Eduardo Costa <m4c0@users.noreply.github.com>
2021-04-28 10:04:40 +0100

    [vulkan] allowing moves of command_buffer_guard

--- vulkan-tools/inc/m4c0/vulkan/command_buffer_guard.hpp
+++ vulkan-tools/inc/m4c0/vulkan/command_buffer_guard.hpp
@@ -10,13 +10,16 @@ namespace m4c0::vulkan::tools {
     explicit constexpr command_buffer_guard(VkCommandBuffer cb) : m_cmd_buf(cb) {
     }
     ~command_buffer_guard() {
-      cmd::end_command_buffer(m_cmd_buf).now();
+      if (m_cmd_buf != nullptr) cmd::end_command_buffer(m_cmd_buf).now();
     }
 
-    command_buffer_guard(command_buffer_guard &&) = delete;
     command_buffer_guard(const command_buffer_guard &) = delete;
-    command_buffer_guard & operator=(command_buffer_guard &&) = delete;
     command_buffer_guard & operator=(const command_buffer_guard &) = delete;
+
+    command_buffer_guard(command_buffer_guard && o) noexcept : m_cmd_buf(o.m_cmd_buf) {
+      o.m_cmd_buf = nullptr;
+    }
+    command_buffer_guard & operator=(command_buffer_guard &&) = delete;
 
     [[nodiscard]] constexpr auto command_buffer() const noexcept {
       return m_cmd_buf;
