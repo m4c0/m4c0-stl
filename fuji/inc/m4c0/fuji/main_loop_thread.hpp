b6085d98a8342b5e2a6b5fdc5a4b39097479688e
Eduardo Costa <m4c0@users.noreply.github.com>
2021-04-29 19:48:07 +0100

    [fuji] type erasure of main_loop_thread

--- fuji/inc/m4c0/fuji/main_loop_thread.hpp
+++ fuji/inc/m4c0/fuji/main_loop_thread.hpp
@@ -7,9 +7,20 @@
 #include <thread>
 
 namespace m4c0::fuji {
+  template<class MainLoopTp>
+  class main_loop_thread;
+
+  /// \brief Base virtual class to allow type erasure of MainLoopTp
+  template<>
+  class main_loop_thread<void> {
+  public:
+    virtual void interrupt() = 0;
+    virtual void window_resized(unsigned w, unsigned h) = 0;
+  };
+
   /// \brief Convenience for running a main_loop in a different thread
   template<class MainLoopTp>
-  class main_loop_thread {
+  class main_loop_thread : public main_loop_thread<void> {
     MainLoopTp m_loop;
     std::thread m_thread;
 
@@ -20,11 +31,11 @@ namespace m4c0::fuji {
       m4c0::log::info("Vulkan thread starting");
     }
 
-    void interrupt() {
+    void interrupt() override {
       m_loop.interrupt();
       m_thread.join();
     }
-    void window_resized(unsigned w, unsigned h) {
+    void window_resized(unsigned w, unsigned h) override {
       m_loop.window_resized(w, h);
     }
   };
