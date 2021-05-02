09b5290733bb2da12433d9681efb643b1ee6a47c
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-02 13:59:46 +0100

    [droid] public link option

--- droid-main/tst/test-link.cpp
+++ droid-main/tst/test-link.cpp
@@ -5,13 +5,5 @@
 void m4c0::droid::handle_command(glue_app * app, glue_command cmd) {
 }
 
-class ANativeActivity;
-extern "C" void ANativeActivity_onCreate(ANativeActivity * activity, void * saved_state, size_t saved_state_size);
-
-int main(int argc, char ** argv) {
-  if (argc < 100) return 0;
-
-  // Just a silly linker hack
-  ANativeActivity_onCreate(nullptr, nullptr, 0);
-  return 0;
+int main() {
 }
