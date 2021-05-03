c00b26c08bbbfe37358a8ffa795ddc6ebaf9e4de
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-03 07:59:40 +0100

    [assets] fixing mistakes in Android code

--- assets/src/simple_asset.android.cpp
+++ assets/src/simple_asset.android.cpp
@@ -1,15 +1,16 @@
 #include "m4c0/assets/simple_asset.hpp"
 
 #include <android/asset_manager.h>
+#include <string>
 
-static auto load_resource(AAssetManager * amgr, const char * name, const char * ext) {
+static auto * load_resource(AAssetManager * amgr, const char * name, const char * ext) {
   auto name_ext = std::string { name } + "." + ext;
   return AAssetManager_open(amgr, name_ext.c_str(), AASSET_MODE_BUFFER);
 }
 
 namespace m4c0::assets {
   class concrete_simple_asset : public simple_asset {
-    AAsset m_data;
+    AAsset * m_data;
 
   public:
     concrete_simple_asset(AAssetManager * amgr, const char * name, const char * ext)
@@ -28,6 +29,6 @@ namespace m4c0::assets {
   };
 
   std::unique_ptr<simple_asset> simple_asset::load(native_ptr_t nptr, const char * name, const char * ext) {
-    return std::make_unique<concrete_simple_asset>(static_cast<AAssetManager>(nptr), name, ext);
+    return std::make_unique<concrete_simple_asset>(reinterpret_cast<AAssetManager *>(nptr), name, ext);
   }
 }
