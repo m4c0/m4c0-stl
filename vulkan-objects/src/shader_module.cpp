f1f3414136adc5ec87b3f0a81af9b59896e3c56a
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-08 13:07:21 +0100

    [vulkan] fixing recursive recursion

--- vulkan-objects/src/shader_module.cpp
+++ vulkan-objects/src/shader_module.cpp
@@ -1,3 +1,4 @@
+#include "m4c0/assets/simple_asset.hpp"
 #include "m4c0/vulkan/loader.hpp"
 #include "m4c0/vulkan/safe_calls.hpp"
 #include "m4c0/vulkan/shader_module.hpp"
@@ -12,7 +13,7 @@ shader_module shader_module::create_from_spv(std::span<const std::uint32_t> spv)
   return shader_module { details::safe_create_d<VkShaderModule>(vkCreateShaderModule, &info) };
 }
 shader_module shader_module::create_from_spv(const m4c0::assets::simple_asset * asset) {
-  return create_from_spv(asset);
+  return create_from_spv(asset->span<std::uint32_t>());
 }
 
 template<>
