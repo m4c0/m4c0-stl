5c7ff2054ad7932591997f360c864d2012d416e1
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-18 12:36:27 +0100

    [objc-wraps] extensible mtk_view

--- objc-wraps/inc/m4c0/objc/mtk_view.hpp
+++ objc-wraps/inc/m4c0/objc/mtk_view.hpp
@@ -4,6 +4,10 @@
 
 namespace m4c0::objc {
   class mtk_view : public ns_object {
+  protected:
+    explicit mtk_view(const char * cls_name) : ns_object(cls_name) {
+    }
+
   public:
     mtk_view() : ns_object("MTKView") {
     }
--- osx-main/src/metal_view.hpp
+++ osx-main/src/metal_view.hpp
@@ -1,9 +1,30 @@
 #pragma once
 
+#include "m4c0/objc/casts.hpp"
 #include "m4c0/objc/mtk_view.hpp"
+
+static constexpr const char * cpp_ivar_name = "m4c0_$$_cpp";
+
+static const char * get_view_name() {
+  static constexpr const char * view_name = "M4C0_$$_osx_main_view";
+
+  static class register_class {
+  public:
+    register_class() {
+      auto * cls = objc_allocateClassPair(objc_getClass("MTKView"), view_name, 0);
+      class_addIvar(cls, cpp_ivar_name, sizeof(void *), alignof(void *), "@");
+      objc_registerClassPair(cls);
+    }
+  } r;
+
+  return view_name;
+}
 
 namespace m4c0::osx::details {
   class view : public m4c0::objc::mtk_view {
   public:
+    view() : mtk_view(get_view_name()) {
+      object_set_ivar(self(), cpp_ivar_name, this);
+    }
   };
 }
