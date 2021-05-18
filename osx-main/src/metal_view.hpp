8d1df9dbb2334a9a02dd004a6ef83b9473ed5983
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-18 19:22:24 +0100

    [osx-main] NSView in CPP

--- osx-main/src/metal_view.hpp
+++ osx-main/src/metal_view.hpp
@@ -6,10 +6,21 @@
 
 namespace m4c0::osx::details {
   class view : public objc::wrapper<view, objc::mtk_view> {
+    void did_move_to_window() {
+    }
+    void remove_from_superview() {
+    }
+
+    void scale_did_change(void * /*notif*/) {
+    }
+
   public:
     view()
       : wrapper([] {
-        return objc::delegated_class_builder<view>("MTKView");
+        return objc::delegated_class_builder<view>("MTKView")
+            .add_method<&view::did_move_to_window>("viewDidMoveToWindow", "v@:")
+            .add_method<&view::remove_from_superview>("removeFromSuperview", "v@:")
+            .add_method<&view::scale_did_change>("scaleDidChange", "v@:@");
       }) {
     }
   };
