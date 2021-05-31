db65d5d39694538d241dabb5e003b3f77ad7bc19
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-31 16:26:41 +0100

    [objc-wraps] more event types

--- objc-wraps/inc/m4c0/objc/ns_event_type.hpp
+++ objc-wraps/inc/m4c0/objc/ns_event_type.hpp
@@ -2,6 +2,14 @@
 
 namespace m4c0::objc {
   enum class ns_event_type : unsigned {
+    mouse_left_down = 1,
+    mouse_left_up = 2,
+    mouse_right_down = 3,
+    mouse_right_up = 4,
     mouse_moved = 5,
+    mouse_left_dragged = 6,
+    mouse_right_dragged = 7,
+    mouse_enter = 8,
+    mouse_exit = 9,
   };
 }
