e7fd3d7694e1c419ac58d6b9a485e301010fcd86
Eduardo Costa <m4c0@users.noreply.github.com>
2021-06-30 14:40:26 +0100

    [casein] Virtual dtor for virtual handler
    duh

--- casein/inc/m4c0/casein/main.hpp
+++ casein/inc/m4c0/casein/main.hpp
@@ -7,6 +7,13 @@
 namespace m4c0::casein {
   class handler {
   public:
+    handler() = default;
+    virtual ~handler() = default;
+
+    handler(handler &&) = delete;
+    handler(const handler &) = delete;
+    handler & operator=(handler &&) = delete;
+    handler & operator=(const handler &) = delete;
   };
   std::unique_ptr<handler> main(const native_handles * nh);
 }
