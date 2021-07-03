8351b0f67ed6cf8b2f45328d55df3ada09cfcb40
Eduardo Costa <m4c0@users.noreply.github.com>
2021-07-03 17:05:36 +0100

    [riff] test seems good enough

--- riff/tst/riff-dump.cpp
+++ riff/tst/riff-dump.cpp
@@ -5,6 +5,8 @@
 #include <type_traits>
 
 class reader {
+  friend class subreader;
+
 protected:
   [[nodiscard]] virtual bool read(void * buffer, unsigned len) = 0;
 
@@ -23,6 +25,28 @@ public:
   }
 };
 
+class subreader : public reader {
+  reader * m_o;
+  unsigned m_start;
+  unsigned m_len;
+
+public:
+  constexpr subreader(reader * o, unsigned start, unsigned len) : m_o(o), m_start(start), m_len(len) {
+  }
+
+  [[nodiscard]] bool read(void * buffer, unsigned len) override {
+    if (m_o->tellg() + len >= m_start + m_len) return false;
+    return m_o->read(buffer, len);
+  }
+  [[nodiscard]] bool seekg(unsigned pos) override {
+    if (pos < 0) return false;
+    if (pos > m_len) return false;
+    return m_o->seekg(m_start + pos);
+  }
+  [[nodiscard]] unsigned tellg() override {
+    return m_o->tellg() - m_start;
+  }
+};
 class fs_reader : public reader {
   std::ifstream & m_ifs;
 
@@ -45,23 +69,57 @@ struct header {
   std::uint32_t fourcc;
   std::uint32_t length;
 };
-static bool read_riff(reader * r) {
+static std::string fourcc_str(std::uint32_t fourcc) {
+  return std::string(static_cast<char *>(static_cast<void *>(&fourcc)), 4);
+}
+static bool read_chunk(reader * r, bool (*callback)(header h, reader * r)) {
   auto h = r->read<header>();
   if (!h) {
-    std::cerr << "Failed to read RIFF header\n";
-    return false;
-  }
-  if (h->fourcc != 'FFIR') {
-    std::cerr << "File may not be RIFF\n";
+    std::cerr << "Failed to read chunk header\n";
     return false;
   }
   auto start = r->tellg();
-  if (!r->seekg(start + h->length)) {
-    std::cerr << "Truncated chunk at " << start << "\n";
+  auto end = start + (h->length + 1U & ~1U);
+  if (!r->seekg(end)) {
+    std::cerr << "Truncated chunk [" << fourcc_str(h->fourcc) << "] - expected len: " << h->length << "\n";
     return false;
   }
-  std::cerr << "RIFF - len=" << h->length << "\n";
+  if (!r->seekg(start)) {
+    std::cerr << "Failure rewinding stream\n";
+    return false;
+  }
+
+  subreader sr { r, start, h->length };
+  return callback(*h, &sr) && r->seekg(end);
+}
+static bool read_list(header h, reader * r) {
+  auto type = r->read<std::uint32_t>();
+  if (!type) {
+    std::cerr << "Failed to read file type\n";
+    return false;
+  }
+
+  std::cerr << fourcc_str(h.fourcc) << " - type=[" << fourcc_str(*type) << "] - len=" << h.length << "\n";
+  while (r->tellg() < h.length) {
+    if (!read_chunk(r, [](auto h, auto * r) {
+          if (h.fourcc == 'TSIL') return read_list(h, r);
+          std::cerr << "  " << fourcc_str(h.fourcc) << " - len=" << h.length << "\n";
+          return true;
+        })) {
+      return false;
+    }
+  }
+  std::cerr << "end list\n";
   return true;
+}
+static bool read_riff(reader * r) {
+  return read_chunk(r, [](header h, reader * r) {
+    if (h.fourcc != 'FFIR') {
+      std::cerr << "File may not be RIFF\n";
+      return false;
+    }
+    return read_list(h, r);
+  });
 }
 
 int main(int argc, char ** argv) {
