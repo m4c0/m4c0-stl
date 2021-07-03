ef69e092c19ff297a42307c1e9211857c7f1ddb4
Eduardo Costa <m4c0@users.noreply.github.com>
2021-07-03 20:58:58 +0100

    [riff] cleaning code a bit

--- riff/tst/riff-dump.cpp
+++ riff/tst/riff-dump.cpp
@@ -1,3 +1,4 @@
+#include "m4c0/log.hpp"
 #include "m4c0/riff/fs_reader.hpp"
 #include "m4c0/riff/reader.hpp"
 #include "m4c0/riff/subreader.hpp"
@@ -17,53 +18,47 @@ struct header {
 static std::string fourcc_str(std::uint32_t fourcc) {
   return std::string(static_cast<char *>(static_cast<void *>(&fourcc)), 4);
 }
+static bool warn(const char * msg) {
+  m4c0::log::warning(msg);
+  return false;
+}
 static bool read_chunk(reader * r, bool (*callback)(header h, reader * r)) {
   auto h = r->read<header>();
-  if (!h) {
-    std::cerr << "Failed to read chunk header\n";
-    return false;
-  }
+  if (!h) return warn("Failed to read chunk header");
+
   auto start = r->tellg();
   auto end = start + (h->length + 1U & ~1U);
-  if (!r->seekg(end)) {
-    std::cerr << "Truncated chunk [" << fourcc_str(h->fourcc) << "] - expected len: " << h->length << "\n";
-    return false;
-  }
-  if (!r->seekg(start)) {
-    std::cerr << "Failure rewinding stream\n";
-    return false;
-  }
+  if (!r->seekg(end)) return warn("Found truncated or misaligned chunk");
+  if (!r->seekg(start)) return warn("Failure rewinding stream");
 
   subreader sr { r, start, h->length };
   return callback(*h, &sr) && r->seekg(end);
 }
-static bool read_list(header h, reader * r) {
+static bool read_list(header h, reader * r, bool (*chunk_callback)(header h, reader * r)) {
   auto type = r->read<std::uint32_t>();
-  if (!type) {
-    std::cerr << "Failed to read file type\n";
-    return false;
-  }
+  if (!type) return warn("Failed to read file type in chunk list");
 
   std::cerr << fourcc_str(h.fourcc) << " - type=[" << fourcc_str(*type) << "] - len=" << h.length << "\n";
   while (r->tellg() < h.length) {
-    if (!read_chunk(r, [](auto h, auto * r) {
-          if (h.fourcc == 'TSIL') return read_list(h, r);
-          std::cerr << "  " << fourcc_str(h.fourcc) << " - len=" << h.length << "\n";
-          return true;
-        })) {
+    if (!read_chunk(r, chunk_callback)) {
       return false;
     }
   }
   std::cerr << "end list\n";
   return true;
 }
+static bool read_list_chunks(auto h, auto * /*r*/) {
+  std::cerr << "  " << fourcc_str(h.fourcc) << " - len=" << h.length << "\n";
+  return true;
+}
+static bool read_riff_chunks(auto h, auto * r) {
+  if (h.fourcc == 'TSIL') return read_list(h, r, read_list_chunks);
+  std::cerr << "  " << fourcc_str(h.fourcc) << " - len=" << h.length << "\n";
+  return true;
+}
 static bool read_riff(reader * r) {
   return read_chunk(r, [](header h, reader * r) {
-    if (h.fourcc != 'FFIR') {
-      std::cerr << "File may not be RIFF\n";
-      return false;
-    }
-    return read_list(h, r);
+    return (h.fourcc == 'FFIR') ? read_list(h, r, read_riff_chunks) : warn("File may not be RIFF\n");
   });
 }
 
