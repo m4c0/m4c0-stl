16c2356201f0677b8602a59a55a4fde82661cc69
Eduardo Costa <m4c0@users.noreply.github.com>
2021-07-15 19:27:11 +0100

    [io] constexpr reader
    Why? Why not? :P

---
+++ io/inc/m4c0/io/ce_reader.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "m4c0/io/reader.hpp"
+
+#include <array>
+
+namespace m4c0::io {
+  template<auto N>
+  class ce_reader : public reader {
+    std::array<uint8_t, N> m_data;
+    unsigned m_pos {};
+
+  public:
+    template<typename... Tp>
+    constexpr explicit ce_reader(Tp... data) : m_data { static_cast<uint8_t>(data)... } {
+    }
+
+    [[nodiscard]] constexpr bool read(void * /*buffer*/, unsigned /*len*/) override {
+      return false;
+    }
+    [[nodiscard]] constexpr std::optional<uint8_t> read_u8() override {
+      if (m_pos == N) return {};
+      return m_data.at(m_pos++);
+    }
+    [[nodiscard]] constexpr std::optional<uint32_t> read_u32() override {
+      if (m_pos + sizeof(uint32_t) > N) return {};
+
+      constexpr const auto u8_bitsize = 8U;
+      constexpr const auto u16_bitsize = 16U;
+
+      unsigned d = *read_u8();
+      unsigned c = *read_u8();
+      unsigned cd = (c << u8_bitsize) | d;
+
+      unsigned b = *read_u8();
+      unsigned a = *read_u8();
+      unsigned ab = (a << u8_bitsize) | b;
+
+      return (ab << u16_bitsize) | cd;
+    }
+    [[nodiscard]] constexpr bool eof() override {
+      return false;
+    }
+    [[nodiscard]] constexpr bool seekg(unsigned pos) override {
+      m_pos += pos;
+      return m_pos < N;
+    }
+    [[nodiscard]] constexpr unsigned tellg() override {
+      return m_pos;
+    }
+  };
+  template<typename... Tp>
+  ce_reader(Tp...) -> ce_reader<sizeof...(Tp)>;
+}
---
+++ io/tst/test-ce-reader.cpp
@@ -0,0 +1,74 @@
+#include "m4c0/io/ce_reader.hpp"
+
+using namespace m4c0::io;
+
+static constexpr const ce_reader png { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D,
+                                       0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+                                       0x08, 0x02, 0x00, 0x00, 0x00, 0x90, 0x77, 0x53, 0xDE, 0x00, 0x00, 0x00,
+                                       0x0C, 0x49, 0x44, 0x41, 0x54, 0x08, 0xD7, 0x63, 0xF8, 0xCF, 0xC0, 0x00,
+                                       0x00, 0x03, 0x01, 0x01, 0x00, 0x18, 0xDD, 0x8D, 0xB0, 0x00, 0x00, 0x00,
+                                       0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82 };
+// Test initial state
+static_assert([] {
+  ce_reader dat = png;
+  return !dat.eof() && (dat.tellg() == 0);
+}());
+
+// Test if we can seek
+static_assert([] {
+  constexpr const auto valid_pos = 10;
+  ce_reader dat = png;
+  return dat.seekg(valid_pos) && !dat.eof() && (dat.tellg() == valid_pos);
+}());
+
+// Test if we cannot seek past end
+static_assert([] {
+  constexpr const auto invalid_pos = 1000;
+  ce_reader dat = png;
+  return !dat.seekg(invalid_pos);
+}());
+
+// Test if we can seek backwards
+static_assert([] {
+  constexpr const auto valid_pos = 10;
+  constexpr const auto valid_rewind = -3;
+  constexpr const auto final_pos = 7;
+  ce_reader dat = png;
+  return dat.seekg(valid_pos) && dat.seekg(valid_rewind) && !dat.eof() && (dat.tellg() == final_pos);
+}());
+
+// Test if we cannot seek before start
+static_assert([] {
+  constexpr const auto invalid_pos = -1;
+  ce_reader dat = png;
+  return !dat.seekg(invalid_pos);
+}());
+
+// Test if it seeks when reading
+static_assert([] {
+  constexpr const auto final_pos = 7;
+
+  ce_reader dat = png;
+  return dat.seekg(3) && dat.read_u32() && dat.tellg() == final_pos;
+}());
+
+// Test if we can read in proper endianness
+static_assert([] {
+  constexpr const auto expected = 13U;
+  constexpr const auto ihdr_pos = 8;
+
+  ce_reader dat = png;
+  return dat.seekg(ihdr_pos) && (dat.read_u32_be() == expected);
+}());
+static_assert([] {
+  return ce_reader('R', 'I', 'F', 'F').read_u32() == 'FFIR';
+}());
+
+// Test if we don't read past the end
+static_assert([] {
+  ce_reader dat('R', 'I', 'F', 'F');
+  return dat.seekg(2) && !dat.read_u32();
+}());
+
+int main() {
+}
