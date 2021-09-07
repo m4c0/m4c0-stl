bf8b410f1d2a007f728d1b62d9c79e56c45f92cf
Eduardo Costa <m4c0@users.noreply.github.com>
2021-09-07 11:41:16 +0100

    [parser] Utilities for type traits
    This will help type-safety of combiners by limiting them to parser
    metadata.

---
+++ parser/inc/m4c0/parser/traits.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "m4c0/parser/concept.hpp"
+
+namespace m4c0::parser {
+  template<typename P>
+  requires is_parser<P>
+  using result_of_t = std::invoke_result_t<P, std::string_view>;
+
+  template<typename P>
+  requires is_parser<P>
+  using type_of_t = typename result_of_t<P>::type;
+}
---
+++ parser/tst/traits.cpp
@@ -0,0 +1,15 @@
+#include "m4c0/parser/traits.hpp"
+
+#include <type_traits>
+
+using namespace m4c0::parser;
+
+static constexpr result<char> pchar(std::string_view /*in*/) {
+  return failure<char>("");
+}
+
+static_assert(std::is_same_v<result<char>, result_of_t<decltype(pchar)>>);
+static_assert(!std::is_same_v<result<int>, result_of_t<decltype(pchar)>>);
+
+static_assert(std::is_same_v<char, type_of_t<decltype(pchar)>>);
+static_assert(!std::is_same_v<int, type_of_t<decltype(pchar)>>);
