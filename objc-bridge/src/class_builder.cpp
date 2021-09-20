3281b948d6667b62c9c0026b6489885e85a09d20
Eduardo Costa <m4c0@users.noreply.github.com>
2021-09-20 18:39:11 +0100

    [objc-bridge] Missing include

--- objc-bridge/src/class_builder.cpp
+++ objc-bridge/src/class_builder.cpp
@@ -1,5 +1,6 @@
 #include "m4c0/objc/class_builder.hpp"
 
+#include <cmath>
 #include <string>
 
 using namespace m4c0::objc;
@@ -15,7 +16,7 @@ class_builder::class_builder(const char * parent)
 }
 
 class_builder & class_builder::add_ivar(const char * name, unsigned int size, unsigned int align, const char * sign) {
-  class_addIvar(static_cast<Class>(m_class), name, size, log2(align), sign);
+  class_addIvar(static_cast<Class>(m_class), name, size, std::log2(align), sign);
   return *this;
 }
 
