5e712b424adb96fb0203f482b22f19ae4c0b324f
Eduardo Costa <m4c0@users.noreply.github.com>
2022-01-03 13:23:27 +0000

    [parser] add a bunch of static_assert(noexcept())

--- parser/tst/combiners.cpp
+++ parser/tst/combiners.cpp
@@ -15,11 +15,12 @@ static constexpr auto succeed(Tp res, input_t rem) {
   return result { success { res }, rem };
 }
 
-static constexpr auto fnp() {
-  return match_range('A', 'Z') & [](char c) {
+static constexpr auto fnp() noexcept {
+  return match_range('A', 'Z') & [](char c) noexcept {
     return c - 'A';
   };
 }
+static_assert(noexcept(fnp()("")));
 static_assert(!fnp()(""));
 static_assert(!fnp()("cZ"));
 static_assert(fnp()("Cz") == succeed(2, "z"));
@@ -27,48 +28,54 @@ static_assert(fnp()("Cz") == succeed(2, "z"));
 static constexpr auto nah(char c) {
   return c != 'a';
 }
+static_assert(noexcept((match('a') & nah)("")));
 static_assert(!(match('a') && nah)("a"));
 static_assert(!(match('a') && nah)("b"));
 static_assert(!(match('b') && nah)("a"));
 static_assert((match('b') && nah)("ba") == succeed('b', "a"));
 
+static_assert(noexcept((match('a') & fnp())("")));
 static_assert(!(match('a') & fnp())(""));
 static_assert(!(match('a') & fnp())("a"));
 static_assert(!(match('a') & fnp())("bA"));
 static_assert((match('a') & fnp())("aAb") == succeed(0, "b"));
+static_assert((match('b') + fnp())("bCa") == succeed('b' + 2, "a"));
 
+static_assert(noexcept((match('a') & 4)("")));
 static_assert((match('a') & 4)("aha") == succeed(4, "ha"));
 static_assert(!(match('a') & 4)("nope"));
 
-static constexpr auto tag_merger(char a, char b) {
-  return a == '<' && b == '>';
-}
-static_assert(combine(match('<'), match('>'), tag_merger)("<>!") == succeed(true, "!"));
-
-static_assert(!(match('a') + fnp())(""));
-static_assert(!(match('a') + fnp())("a"));
-static_assert(!(match('a') + fnp())("bA"));
-static_assert((match('b') + fnp())("bCa") == succeed('b' + 2, "a"));
-
+static_assert(noexcept((3 + fnp())("")));
 static_assert(!(3 + fnp())(""));
 static_assert((3 + fnp())("C") == succeed(3 + 2, ""));
 static_assert((3 + fnp())("CD") == succeed(3 + 2, "D"));
 
+static_assert(noexcept((match('a') | 'b')("")));
 static_assert((match('a') | 'b')("aha") == succeed('a', "ha"));
 static_assert((match('a') | 'b')("nope") == succeed('b', "nope"));
 
+static_assert(noexcept((match('a') | "nok")("")));
 static_assert((match('a') | "nok")("aha") == succeed('a', "ha"));
 static_assert((match('a') | "nok")("nope") == fail<char>("nok", "nope"));
 
+static_assert(noexcept((match('a') | match('c'))("")));
 static_assert(!(match('a') | match('c'))(""));
 static_assert(!(match('a') | match('c'))("bc"));
 static_assert((match('a') | match('c'))("ac") == succeed('a', "c"));
 static_assert((match('a') | match('c'))("c") == succeed('c', ""));
 
+static constexpr auto tag_merger(char a, char b) {
+  return a == '<' && b == '>';
+}
+static_assert(noexcept(combine(match('<'), match('>'), tag_merger)("")));
+static_assert(combine(match('<'), match('>'), tag_merger)("<>!") == succeed(true, "!"));
+
+static_assert(noexcept(skip(match('a'))("")));
 static_assert(!skip(match('a'))(""));
 static_assert(!skip(match('a'))("b"));
 static_assert(skip(match('a'))("ab") == succeed(nil {}, "b"));
 
+static_assert(noexcept((match('a') + skip(match('b')))("")));
 static_assert((match('a') + skip(match('b')))("ab") == succeed('a', ""));
 
 struct cnt {
@@ -80,7 +87,7 @@ static constexpr cnt operator+(cnt a, cnt b) {
 static constexpr bool operator==(cnt a, cnt b) {
   return a.c == b.c;
 }
-static constexpr auto cntp() {
+static constexpr auto cntp() noexcept {
   return match('B') & cnt { 1 };
 }
 struct cnt_init {
@@ -93,6 +100,7 @@ static constexpr bool operator==(cnt_init a, cnt_init b) {
   return a.c == b.c;
 }
 
+static_assert(noexcept((constant(cnt_init {}) << cntp())("")));
 static_assert((constant(cnt_init {}) << cntp())("") == succeed(cnt_init {}, ""));
 static_assert((constant(cnt_init {}) << cntp())("a") == succeed(cnt_init {}, "a"));
 static_assert((constant(cnt_init {}) << cntp())("B") == succeed(cnt_init { 1 }, ""));
@@ -101,6 +109,7 @@ static_assert((constant(cnt_init {}) << cntp())("BBa") == succeed(cnt_init { 2 }
 static_assert((constant(cnt_init {}) << cntp())("BBB") == succeed(cnt_init { 3 }, ""));
 static_assert(!(fail<cnt_init>("") << cntp())(""));
 
+static_assert(noexcept(at_least_one(cntp())("")));
 static_assert(!at_least_one(cntp())(""));
 static_assert(!at_least_one(cntp())("a"));
 static_assert(at_least_one(cntp())("B") == succeed(cnt { 0b1 }, ""));
@@ -111,6 +120,7 @@ static_assert(at_least_one(cntp())("BBBa") == succeed(cnt { 0b111 }, "a")); // N
 static_assert(!at_least_one(cntp(), cnt_init {})(""));
 static_assert(at_least_one(cntp(), cnt_init {})("B") == succeed(cnt_init { 1 }, "")); // NOLINT
 
+static_assert(noexcept(many(cntp())("")));
 static_assert(many(cntp())("") == succeed(cnt {}, ""));
 static_assert(many(cntp())("a") == succeed(cnt {}, "a"));
 static_assert(many(cntp())("B") == succeed(cnt { 0b1 }, ""));
@@ -121,6 +131,7 @@ static_assert(many(cntp(), cnt_init {})("") == succeed(cnt_init {}, ""));
 static_assert(many(cntp(), cnt_init {})("a") == succeed(cnt_init {}, "a"));
 static_assert(many(cntp(), cnt_init {})("B") == succeed(cnt_init { 1 }, ""));
 
+static_assert(noexcept(exactly(0, cntp())("")));
 static_assert(exactly(0, cntp())("") == succeed(cnt {}, ""));
 static_assert(exactly(0, cntp())("a") == succeed(cnt {}, "a"));
 static_assert(!exactly(1, cntp())(""));
@@ -136,17 +147,19 @@ static_assert(exactly(3, cntp())("BBB") == succeed(cnt { 0b111 }, ""));   // NOL
 static_assert(exactly(3, cntp())("BBBa") == succeed(cnt { 0b111 }, "a")); // NOLINT
 static_assert(exactly(3, cntp())("BBBB") == succeed(cnt { 0b111 }, "B")); // NOLINT
 
+static_assert(noexcept(at_most(2, cntp())("")));
 static_assert(at_most(2, cntp())("") == succeed(cnt {}, ""));
 static_assert(at_most(2, cntp())("a") == succeed(cnt {}, "a"));
 static_assert(at_most(2, cntp())("Ba") == succeed(cnt { 0b1 }, "a"));
 static_assert(at_most(2, cntp())("BBa") == succeed(cnt { 0b11 }, "a"));
 static_assert(at_most(2, cntp())("BBBa") == succeed(cnt { 0b11 }, "Ba"));
 
-static constexpr auto repeater() {
+static constexpr auto repeater() noexcept {
   return match_digit() >> [](int d) noexcept {
     return exactly(d, cntp());
   };
 }
+static_assert(noexcept(repeater()("")));
 static_assert(!repeater()(""));
 static_assert(!repeater()("A"));
 static_assert(!repeater()("1"));
@@ -154,6 +167,7 @@ static_assert(!repeater()("2B"));
 static_assert(repeater()("1BBB") == succeed(cnt { 0b1 }, "BB"));
 static_assert(repeater()("2BBB") == succeed(cnt { 0b11 }, "B"));
 
+static_assert(noexcept((match("ok") + look_ahead(match("?")))("??")));
 static_assert(!(match("ok") + look_ahead(match("?")))("??"));
 static_assert(!(match("ok") + look_ahead(match("?")))("ok"));
 static_assert((match("ok") + look_ahead(match("?")))("ok?") == succeed(input_t { "ok" }, "?"));
--- parser/tst/str.cpp
+++ parser/tst/str.cpp
@@ -7,21 +7,25 @@ static constexpr auto succeed(Tp res, input_t rem) {
   return result { success { res }, rem };
 }
 
+static_assert(noexcept(any_char()("")));
 static_assert(!any_char()(""));
 static_assert(any_char()("a") == succeed('a', ""));
 static_assert(any_char()("xy") == succeed('x', "y"));
 
+static_assert(noexcept(match('A')("")));
 static_assert(!match('A')(""));
 static_assert(!match('A')("_"));
 static_assert(match('A')("A") == succeed('A', ""));
 static_assert(match('A')("AB") == succeed('A', "B"));
 
+static_assert(noexcept(match("ABC")("")));
 static_assert(!match("ABC")(""));
 static_assert(!match("ABC")("AB"));
 static_assert(!match("ABC")("AABC"));
 static_assert(match("ABC")("ABC") == succeed(input_t { "ABC" }, ""));
 static_assert(match("ABC")("ABCD") == succeed(input_t { "ABC" }, "D"));
 
+static_assert(noexcept(match_any_of("ABC")("")));
 static_assert(!match_any_of("ABC")(""));
 static_assert(!match_any_of("ABC")("D"));
 static_assert(match_any_of("ABC")("A") == succeed('A', ""));
@@ -30,6 +34,7 @@ static_assert(match_any_of("ABC")("C") == succeed('C', ""));
 static_assert(match_any_of("ABC")("ABC") == succeed('A', "BC"));
 static_assert(match_any_of("ABC")("BC") == succeed('B', "C"));
 
+static_assert(noexcept(match_range('a', 'z')("")));
 static_assert(!match_range('a', 'z')(""));
 static_assert(!match_range('a', 'z')("0"));
 static_assert(match_range('a', 'z')("a") == succeed('a', ""));
@@ -37,6 +42,7 @@ static_assert(match_range('a', 'z')("d") == succeed('d', ""));
 static_assert(match_range('a', 'z')("z") == succeed('z', ""));
 static_assert(match_range('a', 'z')("zaza") == succeed('z', "aza"));
 
+static_assert(noexcept(match_none_of("ABC")("")));
 static_assert(!match_none_of("ABC")(""));
 static_assert(!match_none_of("ABC")("A"));
 static_assert(!match_none_of("ABC")("B"));
@@ -44,5 +50,6 @@ static_assert(!match_none_of("ABC")("C"));
 static_assert(match_none_of("ABC")("D") == succeed('D', ""));
 static_assert(match_none_of("ABC")("EF") == succeed('E', "F"));
 
+static_assert(noexcept(eof()("")));
 static_assert(eof()("") == succeed(nil {}, ""));
 static_assert(!eof()("A"));
